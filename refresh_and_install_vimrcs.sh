b836afc705c4cf9f9552a1905206b1773104c117
hrai <rai.hangjit@gmail.com>
2018-09-21 22:27:11 +1000

    Updated Clone Script

--- refresh_and_install_vimrcs.sh
+++ refresh_and_install_vimrcs.sh
@@ -3,7 +3,7 @@ rm -rf ~/.vim_runtime
 echo 'Finished removing .vim_runtime folder...'
 
 echo 'Cloning vim-files.git...'
-git clone --depth=1 git://github.com/hrai/vim-files.git ~/.vim_runtime
+git clone --depth=1 https://github.com/hrai/vim-files.git ~/.vim_runtime
 echo 'Finished cloning vim-files.git...'
 
 echo 'Installing awesome_vimrc...'
