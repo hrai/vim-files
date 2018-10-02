9e041b9c89d3e4c055c5aa97bd76bd92972218cf
Hangjit Rai <hangjit.rai@ofx.com>
2018-10-03 09:50:48 +1000

    Updated Print Message

--- python_functions/open_gitextensions_commit.py
+++ python_functions/open_gitextensions_commit.py
@@ -3,10 +3,9 @@ try:
     import subprocess
 
     current_dir = vim.eval("expand('<sfile>:p:h')")
-    
 
     subprocess.Popen(["C:\Program Files (x86)\GitExtensions\GitExtensions.exe", "commit", current_dir])
-    print ("Opened GitExtensions commit window" + current_dir)
+    print ("Opening GitExtensions commit window in " + current_dir)
 
 except ImportError:
     print ("Shell not installed")
