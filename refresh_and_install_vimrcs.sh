4640f22638bff2fbb934b38d845e3c692bfcf714
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-06 11:07:36 +1000

    var rename

--- refresh_and_install_vimrcs.sh
+++ refresh_and_install_vimrcs.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo 'How do you want to clone the repo? (s/h)'
-read t
+echo 'How do you want to clone the repo? SSH or HTTPS? (s/h)'
+read input
 
 echo 'Removing .vim_runtime folder...'
 rm -rf ~/.vim_runtime
 echo 'Finished removing .vim_runtime folder...'
 
-if [ "$t" = "s" ]; then
+if [ "$input" = "s" ]; then
     echo 'Cloning vim-files.git via SSH...'
     git clone git@github.com:hrai/vim-files.git ~/.vim_runtime
     echo 'Finished cloning vim-files.git...'
