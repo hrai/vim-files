b6f7f16a75bca4cb0ee835c97e5f1d96671c6968
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-16 09:51:57 +1100

    Updated Installer With Vim-plug Cmd

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -25,9 +25,10 @@ catch
 endtry' > ~/.vimrc
 
 # Removing Vundle
-rm -rf ~/.vim/bundle/Vundle.vim
+rm -rf ~/.vim/autoload/plug.vim
 
 # Cloning Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
