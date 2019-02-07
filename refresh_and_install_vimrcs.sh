c82d8310f6e94551d49f02e4e571df226e616464
Hangjit Rai <rai.hangjit@gmail.com>
2019-02-07 21:16:51 +1100

    Updated Install Script

--- refresh_and_install_vimrcs.sh
+++ refresh_and_install_vimrcs.sh
@@ -7,13 +7,13 @@ echo 'Removing .vim_runtime folder...'
 rm -rf ~/.vim_runtime
 echo 'Finished removing .vim_runtime folder...'
 
-if [ "$input" = "s" ]; then
-    echo 'Cloning vim-files.git via SSH...'
-    git clone git@github.com:hrai/vim-files.git ~/.vim_runtime
-    echo 'Finished cloning vim-files.git...'
-else
+if [ "$input" = "h" ]; then
     echo 'Cloning vim-files.git via HTTPS...'
     git clone https://github.com/hrai/vim-files.git ~/.vim_runtime
+    echo 'Finished cloning vim-files.git...'
+else
+    echo 'Cloning vim-files.git via SSH...'
+    git clone git@github.com:hrai/vim-files.git ~/.vim_runtime
     echo 'Finished cloning vim-files.git...'
 fi
 
