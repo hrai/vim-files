d5d4b82873ee4959d358c5bc5ea9583e768d88d3
Hangjit Rai <hangjit.rai@ofx.com>
2019-03-06 14:09:24 +1100

    Updated Install Scripts

---
+++ .vimrc
@@ -0,0 +1,24 @@
+" start of the content
+
+set runtimepath+=~/.vim
+set runtimepath+=~/.vim_runtime
+
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.vim_runtime/vimrcs/custom_methods.vim
+
+try
+source ~/.vim_runtime/my_configs.vim
+
+if has('python3')
+	source ~/.vim_runtime/vimrcs/python_functions.vim
+else
+	echo ('Please install python 3.5')
+	finish
+endif
+
+catch
+endtry
+
--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -3,29 +3,7 @@ set -e
 
 cd ~/.vim_runtime
 
-echo '
-" start of the content
-set runtimepath+=~/.vim
-set runtimepath+=~/.vim_runtime
-
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
-source ~/.vim_runtime/vimrcs/custom_methods.vim
-
-try
-source ~/.vim_runtime/my_configs.vim
-
-if has('python3')
-	source ~/.vim_runtime/vimrcs/python_functions.vim
-else
-	echo ('Please install python 3.5')
-	finish
-endif
-
-catch
-endtry' > ~/.vimrc
+cp -R .vimrc ~/.vimrc
 
 # Removing vim plug
 rm -rf ~/.vim/autoload/plug.vim
