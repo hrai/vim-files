a05cd5073620bf9b8a46c3348534f88c346ea051
Hangjit Rai <hangjit.rai@outlook.com>
2020-04-25 15:09:07 +1000

    Added Nvim Installer

---
+++ install_nvim_vimrc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+NVIM_FOLDER=~/.config/nvim
+
+mkdir -p $NVIM_FOLDER
+
+cp -R .vimrc $NVIM_FOLDER/init.vim
+
+# Removing vim plug
+rm -rf ~/.vim/autoload/plug.vim
+
+# Cloning vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installed the Ultimate Vim configuration successfully for neovim! Enjoy
+:-)"
