1dac173ad1cca92079f7f972a12d262a8a266406
Hangjit Rai <hangjit.rai@outlook.com>
2020-04-26 13:40:26 +1000

    Added Nvim Config

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -2,7 +2,6 @@
 set -e
 
 cd ~/.vim_runtime
-
 cp -R .vimrc ~/.vimrc
 
 # Removing vim plug
@@ -11,5 +10,14 @@ rm -rf ~/.vim/autoload/plug.vim
 # Cloning vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo 'Setting up neovim config...'
+NVIM_CONFIG=~/.config/nvim/
+mkdir -p $NVIM_CONFIG
+cp -R init.vim $NVIM_CONFIG/
+
+# Cloning vim plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
