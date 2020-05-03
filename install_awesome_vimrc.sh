451dc9ea691fc79d0e9558e6b7f5569d54d81f3e
Hangjit Rai <hangjit.rai@outlook.com>
2020-05-03 15:34:31 +1000

    Added Coc-Settings Setup For Windows

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -17,6 +17,10 @@ mkdir -p $NVIM_CONFIG
 cp -R init.vim $NVIM_CONFIG/
 cp -R coc-settings.json $NVIM_CONFIG/
 
+# for windows
+mkdir -p ~/vimfiles
+cp -R coc-settings.json ~/vimfiles/
+
 # echo 'Cloning vim plug...'
 # curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
