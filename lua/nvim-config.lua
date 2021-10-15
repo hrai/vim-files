431276695bbd29e27404650eff57ef73d6dedb7f
Hangjit Rai <hangjit.rai@outlook.com>
2021-10-15 11:04:47 +1100

    Added Lua Config

---
+++ lua/nvim-config.lua
@@ -0,0 +1,54 @@
+---------------------------------------------------------------
+--=> Treesitter configs
+---------------------------------------------------------------
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+      'bash'; 'css'; 'go'; 'html'; 'javascript'; 'json'; 'c_sharp'; 'tsx'; 'typescript';
+      'dockerfile'; 'yaml'; 'lua'; 'python'; 'graphql'; 'vim'; --'markdown';
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner"
+            }
+        },
+        move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+                [']m'] = '@function.outer',
+                [']]'] = '@class.outer'
+            },
+            goto_next_end = {
+                [']M'] = '@function.outer',
+                [']['] = '@class.outer'
+            },
+            goto_previous_start = {
+                ['[m'] = '@function.outer',
+                ['[['] = '@class.outer'
+            },
+            goto_previous_end = {
+                ['[M'] = '@function.outer',
+                ['[]'] = '@class.outer'
+            }
+        }
+    }
+}
+
+---------------------------------------------------------------
+--=> lukas-reineke/indent-blankline.nvim configs
+---------------------------------------------------------------
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+}
