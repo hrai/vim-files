cdd6f45fe1d2500723ed90f70876754df6637c8b
Hangjit Rai <hangjit.rai@outlook.com>
2023-03-23 07:15:37 +1100

    Cleanup

--- config.lua
+++ config.lua
@@ -2,6 +2,19 @@
  THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
  `lvim` is the global options object
 ]]
+if (package.config:sub(1, 1) == '\\') --if OS is Windows
+then
+  -- Enable powershell as your default shell
+  vim.opt.shell = "pwsh.exe -NoLogo"
+  vim.opt.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.cmd [[
+		  let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		  let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		  set shellquote= shellxquote=
+    ]]
+end
+
 -- vim options
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
