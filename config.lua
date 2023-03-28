9d6caaefc8237c571f77c745ac27dfa3746dbaf4
Hangjit Rai <hangjit.rai@outlook.com>
2023-03-29 10:08:25 +1100

    Cleanup

--- config.lua
+++ config.lua
@@ -428,6 +428,35 @@ lvim.plugins = {
     after = "nvim-treesitter",
     dependencies = "nvim-treesitter/nvim-treesitter",
   },
+  {
+    "kiyoon/treesitter-indent-object.nvim",
+    keys = {
+      {
+        "ai",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer()<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer)",
+      },
+      {
+        "aI",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer(true)<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer, line-wise)",
+      },
+      {
+        "ii",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner()<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, partial range)",
+      },
+      {
+        "iI",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner(true)<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, entire range)",
+      },
+    },
+  },
 }
 
 -- Plugin config
