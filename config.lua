7bcc8c46c386b4be86b0048638c236a169d68117
Hangjit Rai <hangjit.rai@outlook.com>
2023-06-30 11:18:58 +1000

    Cleanup

--- config.lua
+++ config.lua
@@ -126,7 +126,7 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.relativenumber = true
 
-vim.opt.relativenumber = true
+
 vim.cmd([[
 " disable folding of sections like JS functions
 set foldmethod=indent       " manual fold
@@ -379,7 +379,7 @@ lvim.plugins = {
   --     vim.cmd 'Copilot run'
   --   end
   -- },
-  { "marko-cerovac/material.nvim" }, --colorscheme
+  { "marko-cerovac/material.nvim" },   --colorscheme
   { "Pocco81/auto-save.nvim", },
   { "tpope/vim-abolish" },
   { "tpope/vim-repeat" },
@@ -394,13 +394,13 @@ lvim.plugins = {
   { "ggandor/lightspeed.nvim" },
   { "mg979/vim-visual-multi" },
   { "elzr/vim-json",                 ft = 'json' },
-  { "justinmk/vim-gtfo" },            --Go to Terminal or File manager
+  { "justinmk/vim-gtfo" },              --Go to Terminal or File manager
   { "chrisbra/csv.vim",              ft = 'csv' },
-  { "psliwka/vim-smoothie" },         --Smooth scrolling
-  { "tyru/open-browser.vim" },        --Open URLs; gx
-  { "tyru/open-browser-github.vim" }, --:OpenGithubFile
+  { "psliwka/vim-smoothie" },           --Smooth scrolling
+  { "tyru/open-browser.vim" },          --Open URLs; gx
+  { "tyru/open-browser-github.vim" },   --:OpenGithubFile
   {
-    "ethanholz/nvim-lastplace",       --Return to last edit position when opening files (You want this!)
+    "ethanholz/nvim-lastplace",         --Return to last edit position when opening files (You want this!)
     config = function()
       require 'nvim-lastplace'.setup {}
     end,
@@ -427,7 +427,7 @@ lvim.plugins = {
     dependencies = 'hrsh7th/nvim-cmp',
   },
   {
-    "AckslD/nvim-neoclip.lua", --Clipboard manager neovim plugin with telescope integration
+    "AckslD/nvim-neoclip.lua",     --Clipboard manager neovim plugin with telescope integration
     dependencies = {
       { 'kkharji/sqlite.lua',           module = 'sqlite' },
       { 'nvim-telescope/telescope.nvim' },
@@ -559,9 +559,9 @@ require 'nvim-treesitter.configs'.setup {
       -- and should return the mode ('v', 'V', or '<c-v>') or a table
       -- mapping query_strings to modes.
       selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V',  -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
+        ['@parameter.outer'] = 'v',         -- charwise
+        ['@function.outer'] = 'V',          -- linewise
+        ['@class.outer'] = '<c-v>',         -- blockwise
       },
       -- If you set this to `true` (default is `false`) then any textobject is
       -- extended to include preceding or succeeding whitespace. Succeeding
