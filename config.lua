9c6f69a2a149c002abba4cf64af25b6903e6cdcb
Hangjit Rai <hangjit.rai@outlook.com>
2023-12-19 16:31:02 +1100

    Updated Lvim Config

--- config.lua
+++ config.lua
@@ -46,6 +46,7 @@ lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.view.relativenumber = true
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- Automatically install missing parsers when entering buffer
